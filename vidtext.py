02ce69292fd79c506445991776218cedd3322457
Tensor <narutokelechi@gmail.com>
2024-04-20 23:56:07 +0100

    Working video file transcrber

--- vidtext.py
+++ vidtext.py
@@ -1,9 +1,11 @@
 import streamlit as st
+import time
 from transformers import pipeline
 from pytube import YouTube
 from pydub import AudioSegment
 from audio_extract import extract_audio
 import google.generativeai as google_genai
+
 import os
 from dotenv import load_dotenv
 
@@ -18,6 +20,7 @@ st.set_page_config(page_title="VidText")
 
 def youtube_video_downloader(url):
     yt_vid = YouTube(url)
+
     title = yt_vid.title
     vid_dld = (
         yt_vid.streams.filter(progressive=True, file_extension="mp4")
@@ -29,16 +32,14 @@ def youtube_video_downloader(url):
     return vid_dld, title
 
 
-def audio_extraction(video_file, output_format):
-    # temp_filename = video_file.name
-    # video_path = f"{temp_filename}"
-    audio = extract_audio(
-        input_path=os.fspath(video_file),
-        output_path=f"{str(video_file)[:-4]}.mp3",
-        output_format=f"{output_format}",
-    )
-    
-    return audio
+def audio_extraction(video_file):
+    # video_file_bytes = os.fsencode(video_file)
+    # audio = extract_audio(input_path=video_file_bytes, output_path=f"{video_file}.mp3")
+    audio = AudioSegment.from_file(video_file, format="mp4")
+    audio_path = "audio.wav"
+    audio.export(audio_path, format="wav")
+
+    return audio_path
 
 
 def audio_processing(mp3_audio):
@@ -53,12 +54,13 @@ def load_asr_model():
     asr_model = pipeline(
         task="automatic-speech-recognition", model="openai/whisper-large-v3"
     )
-    
     return asr_model
 
 
-def transcribe_video(processed_audio):
-    transcriber_model = load_asr_model()
+transcriber_model = load_asr_model()
+
+
+def transcriber_pass(processed_audio):
     text_extract = transcriber_model(processed_audio)
     return text_extract["text"]
 
@@ -73,53 +75,48 @@ def generate_ai_summary(transcript):
 
 # Streamlit UI
 
-youtube_url_tab, file_select_tab, audio_file_tab = st.tabs(
-    ["Youtube url", "Video file", "Audio file"]
-)
+file_select_tab, audio_file_tab = st.tabs(["Video file", "Audio file"])
 
-with youtube_url_tab:
-    url = st.text_input("Enter the Youtube url")
+# with youtube_url_tab:
+#     url = st.text_input("Enter the Youtube url")
 
-    try:
-        yt_video, title = youtube_video_downloader(url)
-        if url:
-            if st.button("Transcribe", key="yturl"):
-                with st.spinner("Transcribing..."):
-                    audio = audio_extraction(yt_video, "mp3")
-                    audio = audio_processing(audio)
-                    ytvideo_transcript = transcribe_video(audio)
-                st.success(f"Transcription successful")
-                st.write(ytvideo_transcript)
-                if st.button("Generate Summary"):
-                    summary = generate_ai_summary(ytvideo_transcript)
-                    st.write(summary)
 
-    except Exception as e:
-        st.error(f"Enter a valid url: {e}")
+#     yt_video, title = youtube_video_downloader(url)
+#     if url:
+#        if st.button("Transcribe", key="yturl"):
+#            with st.spinner("Transcribing..."):
+#                audio = audio_extraction(yt_video, "mp3")
+#                audio = audio_processing(audio)
+#                ytvideo_transcript = transcriber_pass(audio)
+#            st.success(f"Transcription successful")
+#            st.write(ytvideo_transcript)
+#            # st.write(f'Completed in {run_time}')
+
+#            if st.button("Generate Summary"):
+#               summary = generate_ai_summary(ytvideo_transcript)
+#               st.write(summary)
+
 
 # Video file transcription
 
 with file_select_tab:
-    video_file = st.file_uploader("Upload video file", type="mp4")
-    try:
-        # video_file = video_file.getvalue()
-        with open(video_file, "wb") as f:
-            f.write(video_file.getvalue())
+    uploaded_video_file = st.file_uploader("Upload video file", type="mp4")
 
-        if video_file:
-            if st.button("Transcribe", key="vidfile"):
-                with st.spinner("Transcribing..."):
-                    audio = audio_extraction(video_file, "mp3")
-                    audio = audio_processing(audio)
-                    video_transcript = transcribe_video(audio)
-                    st.success(f"Transcription successful")
-                    st.write(video_transcript)
-                    if st.button("Generate Summary", key="ti2"):
-                        summary = generate_ai_summary(video_transcript)
-                        st.write(summary)
+    if uploaded_video_file:
+        video_file = uploaded_video_file
 
-    except Exception as e:
-        st.error(e)
+        if st.button("Transcribe", key="vidfile"):
+            with st.spinner("Transcribing..."):
+                audio = audio_extraction(uploaded_video_file)
+                # audio = audio_processing(audio)
+                video_transcript = transcriber_pass(audio)
+                st.success(f"Transcription successful")
+                st.write(video_transcript)
+
+                if st.button("Generate Summary", key="ti2"):
+                    summary = generate_ai_summary(video_transcript)
+                    st.write(summary)
+
 
 # Audio transcription
 with audio_file_tab:
@@ -129,7 +126,7 @@ with audio_file_tab:
         if st.button("Transcribe", key="audiofile"):
             with st.spinner("Transcribing..."):
                 processed_audio = audio_processing(audio_file)
-                audio_transcript = transcribe_video(processed_audio)
+                audio_transcript = transcriber_pass(processed_audio)
                 st.success(f"Transcription successful")
                 st.write(audio_transcript)
 
